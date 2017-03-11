b855c8fead25066357f333cf84ff1bff0395ca51
sebBielawski <sebBielawski@users.noreply.github.com>
2017-03-11 17:05:50 +0100

    -enfin parsing paralleliser correct

--- Pdp_Velos/csvdatafilereader.cpp
+++ Pdp_Velos/csvdatafilereader.cpp
@@ -20,93 +20,29 @@ CsvDataFileReader::~CsvDataFileReader()
 
 bool CsvDataFileReader::readData(QVector<Trip>& trips, QVector<Station>& stations) const
 {
+    // try to open a readonly text file
     QFile file(DataFileReader::filename());
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
         return false;
 
+    // sugar syntactic reading and splitting
     QStringList lines = QString(file.readAll()).split('\n');
     if (!lines.isEmpty())
         lines.removeFirst();
 
     file.close();
 
-    QHash<const QString&, Station> stationsMap;
+    // parsing is parallelized so we need locks to synchronize data access
+    QReadWriteLock tripsLock, stationsMapLock;
 
+    // at this point, each station is identified by its name
+    QHash<const QString, Station> stationsMap;
+
+    // clear trips vector (if it's not) and reserve space
     trips.clear();
     trips.reserve(lines.size());
 
-    /*for (QString line : lines)
-    {
-        const QStringList fields = line.remove('"').split(',');
-        if (fields.size() < 11)
-            continue;
-
-        const QString& startName = fields.at(4);
-        const QString& endName = fields.at(8);
-
-        if (startName.isEmpty() || endName.isEmpty())
-            continue;
-
-        const QString& startDateTimeStr = fields.at(1);
-        const QString& endDateTimeStr = fields.at(2);
-        const QString& startLatitudeStr = fields.at(5);
-        const QString& startLongitudeStr = fields.at(6);
-        const QString& endLatitudeStr = fields.at(9);
-        const QString& endLongitudeStr = fields.at(10);
-
-        const auto insertStation = [&stationsMap](const QString& name, const QString& latitudeStr, const QString& longitudeStr)
-        {
-            const auto it = stationsMap.find(name);
-            if (it != stationsMap.end())
-                return it.value();
-            else
-            {
-                Station s;
-                s.name = name;
-                s.id = stationsMap.size() ;
-                s.latitude = latitudeStr.toDouble();
-                s.longitude = longitudeStr.toDouble();
-                stationsMap.insert(name, s);
-                return s;
-            }
-        };
-
-        const Station& startStation = insertStation(startName, startLatitudeStr, startLongitudeStr);
-        const Station& endStation = insertStation(endName, endLatitudeStr, endLongitudeStr);
-
-        Trip t;
-        t.id = trips.size();
-
-        t.startDateTime = QDateTime::fromString(startDateTimeStr, dateFormat());
-        t.endDateTime = QDateTime::fromString(endDateTimeStr, dateFormat());
-
-        t.startStationId = startStation.id;
-        t.endStationId = endStation.id;
-
-        t.distance = startStation.distance(endStation);
-        t.direction = startStation.direction(endStation);
-
-        t.duration = t.startDateTime.secsTo(t.endDateTime);
-        t.isCyclic = (startName == endName);
-
-        if (t.isCyclic)
-            stationsMap[startName].appendCycle(t);
-        else
-        {
-            stationsMap[startName].appendDeparture(t);
-            stationsMap[endName].appendArrival(t);
-        }
-
-        trips.append(t);
-
-        // TODO : a retirer
-        //        if (tripsData.size() >= 1000)
-        //            break;
-    }*/
-
-    QReadWriteLock tripsLock;
-    QReadWriteLock stationsMapLock;
-
+    // the functor that will be run in parallel
     const auto runFunction = [this, &trips, &tripsLock, &stationsMap, &stationsMapLock](QString& line)
     {
         const QStringList fields = line.remove('"').split(',');
@@ -118,6 +54,7 @@ bool CsvDataFileReader::readData(QVector<Trip>& trips, QVector<Station>& station
         if (startName.isEmpty() || endName.isEmpty())
             return;
 
+        const QString& durationStr = fields.at(0);
         const QString& startDateTimeStr = fields.at(1);
         const QString& endDateTimeStr = fields.at(2);
         const QString& startLatitudeStr = fields.at(5);
@@ -166,7 +103,7 @@ bool CsvDataFileReader::readData(QVector<Trip>& trips, QVector<Station>& station
         t.endDateTime = QDateTime::fromString(endDateTimeStr, dateFormat());
         t.distance = startStation.distance(endStation);
         t.direction = startStation.direction(endStation);
-        t.duration = t.startDateTime.secsTo(t.endDateTime);
+        t.duration = durationStr.toDouble();
         t.isCyclic = (startName == endName);
 
         stationsMapLock.lockForWrite();
@@ -184,35 +121,14 @@ bool CsvDataFileReader::readData(QVector<Trip>& trips, QVector<Station>& station
         tripsLock.unlock();
     };
 
-    /*QThreadPool pool;
-    pool.setExpiryTimeout(-1);
-
-    const int linesCount = lines.size();
-    const int threadCount = pool.maxThreadCount();
-    QVector<QStringList> lists;
-    int pos = 0;
-    int len = (linesCount / threadCount);
-    for (int i = 0; i < threadCount; ++i)
-    {
-        lists.append(lines.mid(pos, len));
-        pos += len;
-        if (i == (threadCount - 1) && ((linesCount % threadCount) != 0))
-            len += 1;
-    }
-
-    QVector<QFuture<void>> results;
-    for (const QStringList l : lists)
-        results.append(QtConcurrent::run(runFunction, l));
-
-    for (QFuture<void> r : results)
-        r.waitForFinished();*/
-
+    // parses all lines from file in parallel (Qt does all the work for us and chooses an appropriate threads count)
     QtConcurrent::blockingMap(lines, runFunction);
-
-    stations = stationsMap.values().toVector();
 
     // some trips may be invalids so we didn't insert them into the vector
     trips.squeeze();
+
+    // transmits stations from map to vector
+    stations = stationsMap.values().toVector();
 
     // to optimize memory usage (because QVector allocates more than needed)
     for (Station s : stations)
