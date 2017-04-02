fb3f3bbf078750e19941a6d53973cc6ea9922586
AlaricBr <alaric.braillon@gmail.com>
2017-04-02 18:38:08 +0200

    Ajout tests

---
+++ VisualBSS/VisualBSS/bandit/bandit.h
@@ -0,0 +1,15 @@
+#ifndef BANDIT_BANDIT_H
+#define BANDIT_BANDIT_H
+
+#ifdef _MSC_VER
+#if (_MSC_VER < 1900)
+// Visual Studio versions before 2015 do not support the noexcept keyword
+#define _ALLOW_KEYWORD_MACROS
+#define noexcept
+#endif
+#endif
+
+#include <bandit/grammar.h>
+#include <bandit/runner.h>
+
+#endif
